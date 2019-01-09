2f6ce6359f9347b96ccae8e6f35881bf26a4b25b
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2019-01-09 15:44:27 +0000

    Add licence and use macro in conversion function
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

--- rgb24_15.h
+++ rgb24_15.h
@@ -1,11 +1,36 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org>
+*/
+
 #define RGB24_TO_15(x) ((((x & 0x0000FF) >> 3) & 0x001F) | \
 		(((x & 0x00FF00) >> 6) & 0x03E0) | \
 		(((x & 0xFF0000) >> 9) & 0x7C00))
 
 short rgb24_to_15(long int rgb24)
 {
-	return (((rgb24 & 0x0000FF) >> 3) & 0x001F) |
-		(((rgb24 & 0x00FF00) >> 6) & 0x03E0) |
-		(((rgb24 & 0xFF0000) >> 9) & 0x7C00);
+	return RGB24_TO_15(rgb24); 
 }
 
